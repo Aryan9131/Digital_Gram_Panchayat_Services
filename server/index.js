e366d5452a4a15a8c0ddb2bfb053342301ad10eb
Aryan Nayak <aryannayak9131@gmail.com>
2025-01-12 18:41:48 +0530

    Added Port config

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ admin.initializeApp({
 const db = admin.firestore(); // Use Firestore from Firebase Admin
 
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 // Enable CORS
 const corsOptions = {
